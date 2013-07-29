6e6ba57b7e7d98d82ae2b2aa7d33895a1fd732a8
everm01 <meverha1@gmail.com>
2013-07-29 13:54:45 -0400

    Half Way through Tutorial 3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'mysite.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-TEMPLATE_DIRS = (
+TEMPLATE_DIRS = ('/Volumes/OS_X_Data/Python/django_tutorial/mysite/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
